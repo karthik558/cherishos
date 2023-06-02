d08fd8a9ac36f0ad4ff915cd0ac8bbf0da117d42
KARTHIK LAL <karthik.lal558@gmail.com>
2023-06-02 17:57:27 +0530

    git/website: fix issues after deploying (ui/server_dom)
    * Fixed blur not showing on hamburger-menu (768)
    * Removed duplicate Nav-Bar file cache
    * Fixed issue with mouse_pointer showing on (-768 Devices)
    
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,10 +1,16 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
 import appData from "../../data/app.json";
 import { handleDropdown, handleMobileDropdown } from "../../common/navbar";
 
-const Navbar = ({ lr, nr, theme }) => {
+const NavBar = ({ lr, nr, theme }) => {
+  const handleNavLinkClick = (e) => {
+    e.preventDefault();
+    const targetId = e.target.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <nav
       ref={nr}
@@ -30,10 +36,10 @@ const Navbar = ({ lr, nr, theme }) => {
         <button
           className="navbar-toggler"
           type="button"
-          onClick={handleMobileDropdown}
           data-toggle="collapse"
           data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
+          onClick={handleMobileDropdown}
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
@@ -44,108 +50,51 @@ const Navbar = ({ lr, nr, theme }) => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+            <li className="nav-item">
+              <a className="nav-link" href="#home" onClick={handleNavLinkClick}>
                 Home
-              </span>
-              <div className="dropdown-menu">
-                <Link href={`/homepage/home1-dark`}>
-                  <a className="dropdown-item">Main Home</a>
-                </Link>
-                <Link href={`/homepage/home2-dark`}>
-                  <a className="dropdown-item">Creative Agency</a>
-                </Link>
-                <Link href={`/homepage/home5-dark`}>
-                  <a className="dropdown-item">Digital Agency</a>
-                </Link>
-                <Link href={`/homepage/home4-dark`}>
-                  <a className="dropdown-item">Business One Page</a>
-                </Link>
-                <Link href={`/homepage/home3-dark`}>
-                  <a className="dropdown-item">Corporate Business</a>
-                </Link>
-                <Link href={`/homepage/home6-dark`}>
-                  <a className="dropdown-item">Modern Agency</a>
-                </Link>
-                <Link href={`/homepage/home7-dark`}>
-                  <a className="dropdown-item">Freelancer</a>
-                </Link>
-                <Link href={`/homepage/home8-dark`}>
-                  <a className="dropdown-item">Architecture</a>
-                </Link>
-              </div>
+              </a>
             </li>
             <li className="nav-item">
-              <Link href={`/about/about-dark`}>
-                <a className="nav-link">About</a>
-              </Link>
-            </li>
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
+              <a
+                className="nav-link"
+                href="#features"
+                onClick={handleNavLinkClick}
               >
-                Works
-              </span>
-              <div className="dropdown-menu">
-                <Link href={`/showcase/showcase-dark`}>
-                  <a className="dropdown-item">Showcase Parallax</a>
-                </Link>
-                <Link href={`/showcase4/showcase4-dark`}>
-                  <a className="dropdown-item">Showcase Carousel</a>
-                </Link>
-                <Link href={`/showcase3/showcase3-dark`}>
-                  <a className="dropdown-item">Showcase Circle</a>
-                </Link>
-                <Link href={`/works/works-dark`}>
-                  <a className="dropdown-item">Portfolio Masonry</a>
-                </Link>
-                <Link href={`/works2/works2-dark`}>
-                  <a className="dropdown-item">Portfolio Filtering</a>
-                </Link>
-                <Link href={`/works3/works3-dark`}>
-                  <a className="dropdown-item">Portfolio Gallery</a>
-                </Link>
-              </div>
-            </li>
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Blog
-              </span>
-              <div className="dropdown-menu">
-                <Link href={`/blog/blog-dark`}>
-                  <a className="dropdown-item">Blog Standerd</a>
-                </Link>
-                <Link href={`/blog-list/blog-list-dark`}>
-                  <a className="dropdown-item">Blog List</a>
-                </Link>
-                <Link href={`/blog-grid/blog-grid-dark`}>
-                  <a className="dropdown-item">Blog Grid</a>
-                </Link>
-                <Link href={`/blog-details/blog-details-dark`}>
-                  <a className="dropdown-item">Blog Details</a>
-                </Link>
-              </div>
+                Features
+              </a>
             </li>
             <li className="nav-item">
-              <Link href={`/contact/contact-dark`}>
-                <a className="nav-link">Contact</a>
-              </Link>
+              <a
+                className="nav-link"
+                href="#screenshots"
+                onClick={handleNavLinkClick}
+              >
+                Screenshots
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#team"
+                onClick={handleNavLinkClick}
+              >
+                Team
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="#join-us"
+                onClick={handleNavLinkClick}
+              >
+                Join Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#faq" onClick={handleNavLinkClick}>
+                Faqs
+              </a>
             </li>
           </ul>
         </div>
@@ -154,4 +103,4 @@ const Navbar = ({ lr, nr, theme }) => {
   );
 };
 
-export default Navbar;
+export default NavBar;
